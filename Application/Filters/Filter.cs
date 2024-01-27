717053c46acca994117c45ca911359088d99ce5b
Joseph-Melberg <joseph.melberg@myqops.com>
2024-01-27 15:35:47 -0600

    Removing overlogging

--- Application/Filters/Filter.cs
+++ Application/Filters/Filter.cs
@@ -3,52 +3,41 @@ using MelbergFramework.Infrastructure.Rabbit.Metrics;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Application.Filters;
-    public interface IStopwatch
+public interface IStopwatch
+{
+    long ElapsedMilliseconds { get; }
+    void Start();
+    void Stop();
+    void Reset();
+}
+[AttributeUsage(validOn: AttributeTargets.Class | AttributeTargets.Method)]
+public class ResponseTimeFilter : Attribute, IActionFilter
+{
+    private IActionResponseTimeStopwatch GetStopwatch(HttpContext context)
     {
-        long ElapsedMilliseconds { get; }
-        void Start();
-        void Stop();
-        void Reset();
-        void Report(long ms);
-    }
-    [AttributeUsage(validOn: AttributeTargets.Class | AttributeTargets.Method)]
-    public class ResponseTimeFilter : Attribute, IActionFilter
-    {
-        private IActionResponseTimeStopwatch GetStopwatch(HttpContext context)
-        {
-            return context.RequestServices.GetService<IActionResponseTimeStopwatch>();
-        }
-
-        public void OnActionExecuting(ActionExecutingContext context)
-        {
-            IStopwatch watch = GetStopwatch(context.HttpContext);
-            watch.Reset();
-            watch.Start();
-        }
-        public void OnActionExecuted(ActionExecutedContext context)
-        {
-            IStopwatch watch = GetStopwatch(context.HttpContext);
-            watch.Stop();
-            string value = string.Format("{0}ms", watch.ElapsedMilliseconds);       
-            context.HttpContext.Response.Headers["X-Action-Response-Time"] = value;
-            watch.Report(watch.ElapsedMilliseconds);
-        }
+        return context.RequestServices.GetService<IActionResponseTimeStopwatch>();
     }
 
-    public interface IActionResponseTimeStopwatch : IStopwatch
+    public void OnActionExecuting(ActionExecutingContext context)
     {
+        IStopwatch watch = GetStopwatch(context.HttpContext);
+        watch.Reset();
+        watch.Start();
     }
-
-    public class ActionResponseTimeStopwatch : Stopwatch, IActionResponseTimeStopwatch
+    public void OnActionExecuted(ActionExecutedContext context)
     {
-        private readonly IMetricPublisher _publisher;
-        public ActionResponseTimeStopwatch(IMetricPublisher publisher) : base()
-        {
-            _publisher = publisher;
-        }
-
-    public void Report(long ms)
-    {
-        _publisher.SendMetric("api_response",ms,DateTime.UtcNow);
+        IStopwatch watch = GetStopwatch(context.HttpContext);
+        watch.Stop();
+        string value = string.Format("{0}ms", watch.ElapsedMilliseconds);       
+        context.HttpContext.Response.Headers["X-Action-Response-Time"] = value;
     }
+}
+
+public interface IActionResponseTimeStopwatch : IStopwatch
+{
+}
+
+public class ActionResponseTimeStopwatch : Stopwatch, IActionResponseTimeStopwatch
+{
+    public ActionResponseTimeStopwatch() : base() { }
 }
