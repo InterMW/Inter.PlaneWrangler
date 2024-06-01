11852404d162627fbdf99e16116aebce584dd55c
Joseph-Melberg <josephbmelbergthird@outlook.com>
2024-06-01 17:47:14 +0000

    reducing logs

--- Domain/PlaneFrame.cs
+++ Domain/PlaneFrame.cs
@@ -2,7 +2,7 @@ namespace Domain;
 
 public class PlaneFrame
 {
-    public long Now {get; set;}
+    public long Now {get; set;} = 0;
     public TimeAnotatedPlane[] Planes {get; set;} = Array.Empty<TimeAnotatedPlane>();
     public string Antenna {get; set;} = string.Empty;
     public string Source {get; set;} = string.Empty;
