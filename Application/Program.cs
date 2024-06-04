007d0feaab1ed45196665cafe3be7d5711442e35
Joseph-Melberg <josephbmelbergthird@outlook.com>
2024-06-04 00:53:52 +0000

    all the way back

--- Application/Program.cs
+++ Application/Program.cs
@@ -6,36 +6,41 @@ public class Program
 {
     public static async Task Main(string[] args)
     {
-        ThreadPool.SetMinThreads(8, 8); //required
-        var cors = "CORS";
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
 
-        await MelbergHost
-            .CreateHost<AppRegistrator>()
-            .DevelopmentPasswordReplacement("Rabbit:ClientDeclarations:Connections:0:Password", "rabbit_pass")
-            .AddServices(_ =>
-                    {
-                        _.AddControllers();
-                        _.AddSwaggerGen();
-                        _.AddCors(options =>
-                        {
-                            options.AddPolicy(name: cors,
-                                              policy =>
-                                              {
-                                                  policy.WithOrigins("http://plane.centurionx.net", "https://plane.centurionx.net");
-                                              });
-                        });
-                    })
-            .ConfigureApp(_ =>
-                    {
-                        _.UseSwagger();
-                        _.UseSwaggerUI();
-                        _.UseRouting();
-                        _.UseCors(cors);
-                        _.MapControllers();
-                        _.UseHttpsRedirection();
-                    })
-            .Build()
-            .RunAsync();
+        app.MapGet("/wringler/test", () => "Hello World!");
+
+        app.Run();
+        // ThreadPool.SetMinThreads(8, 8); //required
+        // var cors = "CORS";
+
+        // await MelbergHost
+        //     .CreateHost<AppRegistrator>()
+        //     .DevelopmentPasswordReplacement("Rabbit:ClientDeclarations:Connections:0:Password", "rabbit_pass")
+        //     .AddServices(_ =>
+        //             {
+        //                 _.AddControllers();
+        //                 _.AddSwaggerGen();
+        //                 _.AddCors(options =>
+        //                 {
+        //                     options.AddPolicy(name: cors,
+        //                                       policy =>
+        //                                       {
+        //                                           policy.WithOrigins("http://plane.centurionx.net", "https://plane.centurionx.net");
+        //                                       });
+        //                 });
+        //             })
+        //     .ConfigureApp(_ =>
+        //             {
+        //                 _.UseSwagger();
+        //                 _.UseSwaggerUI();
+        //                 _.UseRouting();
+        //                 _.UseCors(cors);
+        //                 _.MapControllers();
+        //             })
+        //     .Build()
+        //     .RunAsync();
 
     }
 }
